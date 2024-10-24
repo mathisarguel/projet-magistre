c41b58d84750ef026e3e53fbb37f4dffd6f7efc6
annalebrun <anna.lebrun.jd@gmail.com>
2024-10-24 11:53:58 +0200

    calculatrcie
    je fais ce qui est demande vu que t pas la

--- src/test.py
+++ src/test.py
@@ -1,5 +1,27 @@
-def test():
-    print("ok")
-test()
-print("ANNa")jkkjklk
-GROS LARD
+import tkinter as tk
+widown = tk.Tk()
+class calcul:
+    def __init__(self,a,b):
+        self.chif1 = a
+        self.chif2 = b
+        cpt = 0
+
+    def run(self):
+        self.chif1 = int(input("premier chiffre"))
+        self.chif2 = int(input("deuxieme chiffre"))
+
+    def plus(self):
+        self.cpt = self.chif1  + self.chif2
+
+    def moins(self):
+        self.cpt = self.chif1 - self.chif2
+
+    def fois(self):
+        self.cpt = self.chif1 * self.chif2
+
+    def div(self):
+        try:
+            self.cpt = self.chif1/self.chif2
+        except ZeroDivisionError:
+            print("error")
+
